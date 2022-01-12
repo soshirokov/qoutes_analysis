f2b1ac7ea959566f2fe669298c2ec03ac69332c3
soshirokov <soshirokov@gmail.com>
2022-01-12 18:37:47 +0300

    Подкачка котировок из Тинькофф

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,136 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row>
+      <v-autocomplete
+        v-model="selectedTicker"
+        :items="listOfLabels"
+        label="Stocks"
+        auto-select-first
+        chips
+        clearable
+        multiple
+        no-data-text="No results"
+        @change="loadHistory"
+      ></v-autocomplete>
+      {{ instrumentsWithCandles }}
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+const TOKEN = require("../configs/token.json");
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    instruments: [],
+    selectedTicker: [],
+    token: TOKEN.token,
+    headers: {
+      Authorization:
+        "Bearer t.wRRBTQNQTifIDMkXK4ykpMKAHqZGhYZWEE5TKvk33UjA3JrMszrvzc3fjbtG6eeJ8ZdVYsvA8jldjg81ELQhAQ",
+      "Content-type": "application/json",
+    },
+  }),
+
+  methods: {
+    ticketsFetch() {
+      const URL = "https://api-invest.tinkoff.ru/openapi/sandbox/market/stocks";
+      const OPTIONS = {
+        method: "GET",
+        headers: this.headers,
+      };
+
+      this.fetchData(URL, OPTIONS).then((result) =>
+        result.payload.instruments.forEach(({ figi, ticker }) =>
+          this.instruments.push({ figi: figi, ticker: ticker })
+        )
+      );
+    },
+
+    fetchData(URL, OPTIONS) {
+      return fetch(URL, OPTIONS).then((response) => response.json());
+    },
+
+    loadHistory() {
+      this.selectedTicker.forEach((ticker) => {
+        const checkCandles = this.checkCandles(ticker);
+
+        if (checkCandles) return;
+
+        this.loadHistoryByTicker(ticker);
+      });
+    },
+
+    loadHistoryByTicker(ticker) {
+      const URL =
+        "https://api-invest.tinkoff.ru/openapi/sandbox/market/candles";
+      const OPTIONS = {
+        method: "GET",
+        headers: this.headers,
+      };
+      const PARAMS = {
+        figi: this.getFigiByTicker(ticker),
+        from: new Date(2016, 0, 1, 0, 0, 0, 0).toISOString(),
+        to: new Date().toISOString(),
+        interval: "month",
+      };
+
+      const urlParams = URL + "?" + new URLSearchParams(PARAMS).toString();
+
+      this.fetchData(urlParams, OPTIONS).then((result) => {
+        this.$set(
+          this.instruments[
+            this.instruments.findIndex(
+              (instrument) => instrument.figi == result.payload.figi
+            )
+          ],
+          "candles",
+          result.payload.candles.map((candle) => {
+            return {
+              close: candle.c,
+              time:
+                this.formatDate(new Date(candle.time).getDate()) +
+                "." +
+                this.formatDate(new Date(candle.time).getMonth() + 1) +
+                "." +
+                new Date(candle.time).getFullYear(),
+            };
+          })
+        );
+      });
+    },
+
+    getFigiByTicker(ticker) {
+      return this.instruments.filter((item) => item.ticker == ticker)[0].figi;
+    },
+
+    checkCandles(ticker) {
+      return this.instruments[
+        this.instruments.findIndex((instrument) => instrument.ticker == ticker)
+      ].candles?.length;
+    },
+
+    formatDate(date) {
+      return date.toString().length > 1 ? date : "0" + date;
+    },
+  },
+
+  computed: {
+    listOfLabels() {
+      if (this.instruments.length === 0) return [];
+      return this.instruments.map((item) => item.ticker).sort();
+    },
+    instrumentsWithCandles() {
+      return this.instruments.filter((instrument) =>
+        this.checkCandles(instrument.ticker)
+      );
+    },
+  },
+
+  created() {
+    this.ticketsFetch();
+  },
+};
+</script>
